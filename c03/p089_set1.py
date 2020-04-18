a2362d122a5cb8055530ee35195f81fb528f5ad7
moqi <moqimoqidea@gmail.com>
2020-04-18 22:12:14 +0800

    20200418

--- c03/p089_set1.py
+++ c03/p089_set1.py
@@ -11,3 +11,28 @@ print(b_set)
 <class 'set'>
 {1, 2}
 """
+
+a_list = ['a', 'b', 'mpilgrim', 'z', 'example']
+c_set = set(a_list)
+
+print(c_set)
+print(a_list)
+
+"""
+{'b', 'a', 'example', 'z', 'mpilgrim'}
+['a', 'b', 'mpilgrim', 'z', 'example']
+"""
+
+d_set = set()
+print(d_set)
+print(type(d_set))
+print(len(d_set))
+not_sure = {}
+print(type(not_sure))
+
+"""
+set()
+<class 'set'>
+0
+<class 'dict'>
+"""
