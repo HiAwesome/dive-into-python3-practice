fb21df63b812d1c120453331939c52d7be3a6d8b
moqi <moqimoqidea@gmail.com>
2020-04-18 10:39:15 +0800

    20200418

---
+++ c03/p072_list1.py
@@ -0,0 +1,33 @@
+a_list = ['a', 'b', 'mpilgrim', 'z', 'example']
+
+print(a_list)
+print(a_list[0])
+print(a_list[4])
+print(a_list[-1])
+print(a_list[-3])
+
+"""
+['a', 'b', 'mpilgrim', 'z', 'example']
+a
+example
+example
+mpilgrim
+"""
+
+print(a_list[1:3])
+print(a_list[1:-1])
+print(a_list[0:3])
+print(a_list[:3])
+print(a_list[3:])
+print(a_list[:])
+
+"""
+['b', 'mpilgrim']
+['b', 'mpilgrim', 'z']
+['a', 'b', 'mpilgrim']
+['a', 'b', 'mpilgrim']
+['z', 'example']
+['a', 'b', 'mpilgrim', 'z', 'example']
+"""
+
+
