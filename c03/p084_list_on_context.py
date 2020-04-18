f76e4b4e71f04414c3f5d08e5d654175fb627e1c
moqi <moqimoqidea@gmail.com>
2020-04-18 11:56:26 +0800

    20200418

---
+++ c03/p084_list_on_context.py
@@ -0,0 +1,15 @@
+from is_it_true import is_it_true
+
+is_it_true([])
+is_it_true(['a'])
+is_it_true([False])
+
+"""
+/usr/local/bin/python3 /Users/moqi/Documents/Code/dive-into-python3-practice/c03/p084_list_on_context.py
+no, it is false
+yes, it is true
+yes, it is true
+
+Process finished with exit code 0
+
+"""
