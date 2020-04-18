d09d334b9a4f477b51f15c3040b18fdaad4bb88b
moqi <moqimoqidea@gmail.com>
2020-04-18 22:38:24 +0800

    20200418

--- c03/p100_dic1.py
+++ c03/p100_dic1.py
@@ -4,14 +4,26 @@ print(a_dict)
 print(a_dict['db'])
 print(a_dict['server'])
 
-print(a_dict['mysql'])
+try:
+    print(a_dict['mysql'])
+except KeyError as err:
+    print('The key %s is not exist now' % err)
+
+print()
+a_dict['user'] = 'mark'
+print(a_dict)
+a_dict['user'] = 'dora'
+print(a_dict)
+a_dict['User'] = 'Tom Smith'
+print(a_dict)
 
 """
 {'server': 'db.div.org', 'db': 'mysql'}
 mysql
 db.div.org
-Traceback (most recent call last):
-  File "/Users/moqi/Documents/Code/dive-into-python3-practice/c03/p100_dic1.py", line 7, in <module>
-    print(a_dict['mysql'])
-KeyError: 'mysql'
+The key 'mysql' is not exist now
+
+{'server': 'db.div.org', 'db': 'mysql', 'user': 'mark'}
+{'server': 'db.div.org', 'db': 'mysql', 'user': 'dora'}
+{'server': 'db.div.org', 'db': 'mysql', 'user': 'dora', 'User': 'Tom Smith'}
 """
