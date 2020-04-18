78efc58d7340aea8fb87c533e16f0ad91ee756b5
moqi <moqimoqidea@gmail.com>
2020-04-18 10:27:08 +0800

    20200418

---
+++ c03/p067_test_number.py
@@ -0,0 +1,32 @@
+print(type(1))
+print(isinstance(1, int))
+print(1 + 1)
+print(1 + 1.0)
+print(type(2.0))
+
+print()
+
+print(float(2))
+print(int(2.0))
+print(int(2.5))
+print(int(-2.5))
+print(1.12345678901234567890)
+print(type(10000000000000000000000))
+
+"""
+/usr/local/bin/python3 /Users/moqi/Documents/Code/dive-into-python3-practice/c03/p067_test_number.py
+<class 'int'>
+True
+2
+2.0
+<class 'float'>
+
+2.0
+2
+2
+-2
+1.1234567890123457
+<class 'int'>
+
+Process finished with exit code 0
+"""
