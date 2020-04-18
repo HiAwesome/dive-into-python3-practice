fc78f2ead64fd871db4df42380136bab5a09d97d
moqi <moqimoqidea@gmail.com>
2020-04-18 10:32:11 +0800

    20200418

---
+++ c03/p071_math1.py
@@ -0,0 +1,15 @@
+import math
+
+print(math.pi)
+print(math.sin(math.pi / 2))
+print(math.tan(math.pi / 4))
+
+"""
+/usr/local/bin/python3 /Users/moqi/Documents/Code/dive-into-python3-practice/c03/p071_math1.py
+3.141592653589793
+1.0
+0.9999999999999999
+
+Process finished with exit code 0
+
+"""
