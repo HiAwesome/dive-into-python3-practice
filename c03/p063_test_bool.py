1a583d08228e0f1599e33f9adcada3e9892246cf
moqi <moqimoqidea@gmail.com>
2020-04-18 10:24:14 +0800

    20200418

---
+++ c03/p063_test_bool.py
@@ -0,0 +1,27 @@
+size = 1
+
+print(size < 0)
+
+size = 0
+print(size < 0)
+
+size = -1
+print(size < 0)
+
+print(True + True)
+print(True - False)
+print(True * False)
+print(True / False)
+
+"""
+False
+False
+True
+2
+1
+0
+Traceback (most recent call last):
+  File "/Users/moqi/Documents/Code/dive-into-python3-practice/c03/p063_test_bool.py", line 14, in <module>
+    print(True / False)
+ZeroDivisionError: division by zero
+"""
