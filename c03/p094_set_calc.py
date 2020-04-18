ea0e2a26ff296090394f39d29491af2c7bb59a6a
moqi <moqimoqidea@gmail.com>
2020-04-18 22:26:41 +0800

    20200418

---
+++ c03/p094_set_calc.py
@@ -0,0 +1,35 @@
+a_set = {2, 3, 4, 5, 6, 196}
+print(a_set)
+print(3 in a_set)
+print(31 in a_set)
+print()
+
+b_set = {4, 5, 6, 900}
+print(a_set.union(b_set))
+print(a_set.intersection(b_set))
+print(a_set.difference(b_set))
+print(a_set.symmetric_difference(b_set))
+print()
+
+print(b_set.symmetric_difference(a_set))
+print(b_set.symmetric_difference(a_set) == a_set.symmetric_difference(b_set))
+print(b_set.union(a_set) == a_set.union(b_set))
+print(b_set.intersection(a_set) == a_set.intersection(b_set))
+print(b_set.difference(a_set) == a_set.difference(b_set))
+
+"""
+{2, 3, 4, 5, 6, 196}
+True
+False
+
+{2, 3, 4, 5, 6, 196, 900}
+{4, 5, 6}
+{2, 3, 196}
+{2, 3, 900, 196}
+
+{2, 3, 196, 900}
+True
+True
+True
+False
+"""
