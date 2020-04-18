d99da5ecf10aebee8a2b5deb83a89d29d95c7bc8
moqi <moqimoqidea@gmail.com>
2020-04-18 10:07:26 +0800

    20200418

---
+++ c01/p050_test_sys.py
@@ -0,0 +1,23 @@
+import sys, pprint
+
+pprint.pprint(sys.path)
+print()
+pprint.pprint(sys)
+
+"""
+['/Users/moqi/Documents/Code/dive-into-python3-practice/c01',
+ '/Users/moqi/Documents/Code/dive-into-python3-practice',
+ '/Users/moqi/Library/Application '
+ 'Support/JetBrains/Toolbox/apps/PyCharm-P/ch-0/201.6668.28/PyCharm 2020.1 '
+ 'EAP.app/Contents/plugins/python/helpers/pycharm_display',
+ '/usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/lib/python37.zip',
+ '/usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/lib/python3.7',
+ '/usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/lib/python3.7/lib-dynload',
+ '/Users/moqi/Library/Python/3.7/lib/python/site-packages',
+ '/usr/local/lib/python3.7/site-packages',
+ '/Users/moqi/Library/Application '
+ 'Support/JetBrains/Toolbox/apps/PyCharm-P/ch-0/201.6668.28/PyCharm 2020.1 '
+ 'EAP.app/Contents/plugins/python/helpers/pycharm_matplotlib_backend']
+
+<module 'sys' (built-in)>
+"""
