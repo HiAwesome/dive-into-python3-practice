5cc31581eb3dc83cee876d7f7d4891a5f28ec335
moqi <moqimoqidea@gmail.com>
2020-04-18 10:28:56 +0800

    20200418

---
+++ c03/p067_test_number_calc.py
@@ -0,0 +1,19 @@
+print(11 / 2)
+print(11 // 2)
+print(-11 // 2)
+print(11.0 // 2)
+print(11 ** 2)
+print(11 % 2)
+
+"""
+/usr/local/bin/python3 /Users/moqi/Documents/Code/dive-into-python3-practice/c03/p067_test_number_calc.py
+5.5
+5
+-6
+5.0
+121
+1
+
+Process finished with exit code 0
+
+"""
