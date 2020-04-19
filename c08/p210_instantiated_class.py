fa861e1f605ebdb19b466451bc3cd020c1e592f0
moqi <moqimoqidea@gmail.com>
2020-04-19 23:15:28 +0800

    20200419

---
+++ c08/p210_instantiated_class.py
@@ -0,0 +1,12 @@
+import c08.p209_fib_class as p209_fib
+
+fib = p209_fib.Fib(100)
+print(fib)
+print(fib.__class__)
+print(fib.__doc__)
+
+"""
+<c08.p209_fib_class.Fib object at 0x101bd4fd0>
+<class 'c08.p209_fib_class.Fib'>
+ 生成斐波拉切数列的迭代器
+"""
---
+++ c08/p211_instantiated_variable.py
@@ -0,0 +1,12 @@
+import c08.p209_fib_class as p209_fib
+
+fib1 = p209_fib.Fib(100)
+fib2 = p209_fib.Fib(200)
+
+print(fib1.ele)
+print(fib2.ele)
+
+"""
+100
+200
+"""
