3204b69d5ffd7b1de64ae4a4512afdf16f6b595b
moqi <moqimoqidea@gmail.com>
2020-04-19 16:30:07 +0800

    20200419

--- c06/p161_address_regular.py
+++ c06/p161_address_regular.py
@@ -11,9 +11,27 @@ print(s[:-4] + s[-4:].replace('ROAD', 'RD.'))
 
 print(re.sub('ROAD$', 'RD.', s))
 
+print()
+s = '100 BROAD'
+print(re.sub('ROAD$', 'RD.', s))
+print(re.sub('\\bROAD$', 'RD.', s))
+print(re.sub('\bROAD$', 'RD.', s))
+
+print()
+s = '100 BROAD ROAD APT. 3'
+print(re.sub(r'\bROAD$', 'RD.', s))
+print(re.sub(r'\bROAD\b', 'RD.', s))
+
 """
 100 NORTH MAIN RD.
 100 NORTH BRD. RD.
 100 NORTH BROAD RD.
 100 NORTH BROAD RD.
+
+100 BRD.
+100 BROAD
+100 BROAD
+
+100 BROAD ROAD APT. 3
+100 BROAD RD. APT. 3
 """
