1443ddbf7f4a4475037a76bf45f9bd9d8c02317d
moqi <moqimoqidea@gmail.com>
2020-04-19 16:10:11 +0800

    20200419

---
+++ c05/p146_string_slice.py
@@ -0,0 +1,18 @@
+a_string = 'My alphabet starts where your alphabet ends.'
+
+print(a_string[3:11])
+print(a_string[3:-3])
+print(a_string[0:2])
+print(a_string[:18])
+print(a_string[18:])
+
+"""
+/usr/local/bin/python3 /Users/moqi/Documents/Code/dive-into-python3-practice/c05/p146_string_slice.py
+alphabet
+alphabet starts where your alphabet en
+My
+My alphabet starts
+ where your alphabet ends.
+
+Process finished with exit code 0
+"""
