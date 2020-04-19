af99147a5bd61a4093fc5883caac2ad0c18622fc
moqi <moqimoqidea@gmail.com>
2020-04-19 08:54:28 +0800

    20200419

---
+++ c04/p108_os1.py
@@ -0,0 +1,11 @@
+import os
+
+print(os.getcwd())
+
+os.chdir('/Users/moqi/Downloads/output')
+print(os.getcwd())
+
+"""
+/Users/moqi/Documents/Code/dive-into-python3-practice/c04
+/Users/moqi/Downloads/output
+"""
