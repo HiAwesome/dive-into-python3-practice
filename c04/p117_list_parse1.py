5934929ecda38b91a744a75cb7954fd8b5a87d87
moqi <moqimoqidea@gmail.com>
2020-04-19 09:46:06 +0800

    20200419

---
+++ c04/p117_list_parse1.py
@@ -0,0 +1,15 @@
+a_list = [1, 9, 8, 4]
+print([elem * 2 for elem in a_list])
+print(a_list)
+
+b_list = [elem * 3 for elem in a_list]
+print(b_list)
+
+"""
+/usr/local/bin/python3 /Users/moqi/Documents/Code/dive-into-python3-practice/c04/p117_list_parse1.py
+[2, 18, 16, 8]
+[1, 9, 8, 4]
+[3, 27, 24, 12]
+
+Process finished with exit code 0
+"""
