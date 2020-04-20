a10243694aa6c4510e79996c1546c979a915d7a7
moqi <moqimoqidea@gmail.com>
2020-04-20 22:42:04 +0800

    20200420

--- c09/p240_favorite_people.py
+++ c09/p240_favorite_people.py
@@ -2,6 +2,11 @@ names = list(open('../resource/txt/favorite-people.txt', encoding='utf-8'))
 print(names)
 print()
 
+"""
+不幸的是，(对于这个例子来说),list(open(filename))表达式返回的每一行的末尾都包含回车。
+这个列表解析使用rstrip()字符串方法移除每一行尾部的空白。
+(字符串也有一个lstrip()方法移除头部的空白，以及strip()方法头尾都移除。)
+"""
 names = [name.rstrip() for name in names]
 print(names)
 print()
