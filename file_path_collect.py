3f279a0716a4bcedc709202b149841b2278927a1
moqi <moqimoqidea@gmail.com>
2020-04-23 23:05:45 +0800

    20200423

--- file_path_collect.py
+++ file_path_collect.py
@@ -1,9 +1,11 @@
-
 chinese_txt_path = '../resource/txt/chinese.txt'
 
 favorite_people_txt_path = '../resource/txt/favorite-people.txt'
 
-
 output_txt_path_dir = '../resource/txt/output/'
 
 beauregard_path = '../resource/image/beauregard.jpg'
+
+feed_xml_path = '../resource/xml/feed.xml'
+feed_broken_xml_path = '../resource/xml/feed-broken.xml'
+feed_ns0_xml_path = '../resource/xml/feed-ns0.xml'
