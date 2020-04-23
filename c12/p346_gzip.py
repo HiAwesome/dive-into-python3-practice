1327885a6a88b940ba985865ad95b12e62949793
moqi <moqimoqidea@gmail.com>
2020-04-23 22:32:11 +0800

    20200423

--- c12/p346_gzip.py
+++ c12/p346_gzip.py
@@ -6,3 +6,11 @@ gz_file = dir + 'out.log.gz'
 
 with gzip.open(gz_file, mode='wb') as z_file:
     z_file.write('A nine mile walk is no joke, especially in the rain.'.encode('utf-8'))
+
+"""
+生成gz文件后进入目录运行：
+gunzip out.log.gz
+cat out.log
+
+A nine mile walk is no joke, especially in the rain.
+"""
