9b5e87c7d9371eb0741ddd33fa4d16fe3e3859ea
moqi <moqimoqidea@gmail.com>
2020-04-23 22:38:53 +0800

    20200423

---
+++ c12/p348_sys_out.py
@@ -0,0 +1,23 @@
+import sys
+
+for i in range(3):
+    print('PapayaWhip')
+print()
+
+for i in range(3):
+    sys.stdout.write('is the ')
+print()
+
+for i in range(3):
+    # 目前是棕色🏾
+    sys.stderr.write('new black ')
+
+"""
+PapayaWhip
+PapayaWhip
+PapayaWhip
+
+is the is the is the 
+
+new black new black new black 
+"""
