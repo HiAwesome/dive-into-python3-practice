f3cfee27fbf66a8b01dc9cac9245f2971214d629
moqi <moqimoqidea@gmail.com>
2020-04-23 22:19:43 +0800

    20200423

--- c12/p341_open_image.py
+++ c12/p341_open_image.py
@@ -9,9 +9,26 @@ try:
     print(an_image.encoding)
 except AttributeError as err:
     print(err)
+print()
+
+print(an_image.tell())
+data = an_image.read(3)
+print(data)
+print(type(data))
+print(an_image.tell())
+print(an_image.seek(0))
+data = an_image.read()
+print(len(data))
 
 """
 rb
 ../resource/image/beauregard.jpg
 '_io.BufferedReader' object has no attribute 'encoding'
+
+0
+b'\xff\xd8\xff'
+<class 'bytes'>
+3
+0
+3150
 """
