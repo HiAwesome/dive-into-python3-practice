59739840e0b2c945c35901bd58e94e965640f6fd
moqi <moqimoqidea@gmail.com>
2020-04-24 09:30:40 +0800

    20200424

---
+++ c13/p373_find_ele_in_xml.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as etree
+from pprint import pprint
+
+from file_path_collect import feed_xml_path as path
+
+tree = etree.parse(path)
+root = tree.getroot()
+
+pprint(root.findall('{http://www.w3.org/2005/Atom}entry'))
+print(root.tag)
+print(root.findall('{http://www.w3.org/2005/Atom}feed'))
+print(root.findall('{http://www.w3.org/2005/Atom}author'))
+print()
+
+"""
+[<Element '{http://www.w3.org/2005/Atom}entry' at 0x1016a7590>,
+ <Element '{http://www.w3.org/2005/Atom}entry' at 0x1016a7e90>,
+ <Element '{http://www.w3.org/2005/Atom}entry' at 0x1016aa350>]
+{http://www.w3.org/2005/Atom}feed
+[]
+[]
+"""
