9e13cb9cdd1c809894d56292ee1777b5d68d78ab
moqi <moqimoqidea@gmail.com>
2020-04-24 09:32:12 +0800

    20200424

--- c13/p373_find_ele_in_xml.py
+++ c13/p373_find_ele_in_xml.py
@@ -12,11 +12,20 @@ print(root.findall('{http://www.w3.org/2005/Atom}feed'))
 print(root.findall('{http://www.w3.org/2005/Atom}author'))
 print()
 
+pprint(tree.findall('{http://www.w3.org/2005/Atom}entry'))
+print(tree.findall('{http://www.w3.org/2005/Atom}author'))
+print()
+
 """
 [<Element '{http://www.w3.org/2005/Atom}entry' at 0x1016a7590>,
  <Element '{http://www.w3.org/2005/Atom}entry' at 0x1016a7e90>,
  <Element '{http://www.w3.org/2005/Atom}entry' at 0x1016aa350>]
 {http://www.w3.org/2005/Atom}feed
 []
+[]
+
+[<Element '{http://www.w3.org/2005/Atom}entry' at 0x10f8cf650>,
+ <Element '{http://www.w3.org/2005/Atom}entry' at 0x10f8cff50>,
+ <Element '{http://www.w3.org/2005/Atom}entry' at 0x10f8d2410>]
 []
 """
