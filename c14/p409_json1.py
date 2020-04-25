13d89e01a9dd6013ffd7fb0c32a2f095f4a050d9
moqi <moqimoqidea@gmail.com>
2020-04-25 12:30:05 +0800

    20200425

--- c14/p409_json1.py
+++ c14/p409_json1.py
@@ -12,4 +12,13 @@ basic_entry = {
 
 with open(output + 'basic.json', mode='w', encoding='utf-8') as f:
     json.dump(basic_entry, f)
-print('json dump successful.')
+print('basic json dump successful.')
+
+"""
+如果你给json.dump()函数传入indent参数,它以文件变大为代价使生成的JSON文件更可读。
+indent参数是一个整数。0意味着“每个值单独一行。
+”大于0的数字意味着“每个值单独一行并且使用这个数目的空格来缩进嵌套的数据结构。”
+"""
+with open(output + 'basic_with_indent.json', mode='w', encoding='utf-8') as f:
+    json.dump(basic_entry, f, indent=2)
+print('basic_with_indent json dump successful.')
