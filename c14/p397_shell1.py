d38d7ef8fc497e23351faf7c41d08cd7992ec995
moqi <moqimoqidea@gmail.com>
2020-04-25 11:38:08 +0800

    20200425

--- c14/p397_shell1.py
+++ c14/p397_shell1.py
@@ -15,8 +15,18 @@ print(entry['published_date'])
 with open(output + 'entry.pickle', 'wb') as f:
     pickle.dump(entry, f)
 print('pickle save done.')
+print()
+
+# 序列化到内存中
+b = pickle.dumps(entry)
+print(type(b))
+entry3 = pickle.loads(b)
+print(entry == entry3)
 
 """
 time.struct_time(tm_year=2009, tm_mon=3, tm_mday=27, tm_hour=22, tm_min=20, tm_sec=42, tm_wday=4, tm_yday=86, tm_isdst=-1)
 pickle save done.
+
+<class 'bytes'>
+True
 """
