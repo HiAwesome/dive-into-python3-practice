c28a8d3b7a2268e1c119dd04ed333f8cc40592bc
moqi <moqimoqidea@gmail.com>
2020-04-26 10:13:27 +0800

    20200426

--- c14/p419_json_wrong3.py
+++ c14/p419_json_wrong3.py
@@ -1,7 +1,7 @@
 import json
 import time
 
-from c14.p415_customerializer import to_json
+from c14.p418_customerializer2 import to_json
 from file_path_collect import output_json_path_dir as json_output
 
 entry = {'title': 'Divve into history, 2009 edition',
@@ -10,7 +10,6 @@ entry = {'title': 'Divve into history, 2009 edition',
          'published': True, 'published_date': time.strptime('Fri Mar 27 22:20:42 2009')}
 
 with open(json_output + 'entry.json', 'w', encoding='utf-8') as f:
-    # time 对象现在已经可以被 dump 了，是一个 int 数组
     json.dump(entry, f, default=to_json)
 
 """
