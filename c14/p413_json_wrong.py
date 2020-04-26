b542071afee48c2a00b36f9eb2022f42acb4ea68
moqi <moqimoqidea@gmail.com>
2020-04-26 10:08:51 +0800

    20200426

--- c14/p413_json_wrong.py
+++ c14/p413_json_wrong.py
@@ -1,20 +1,20 @@
 import json
-import pickle
-from pprint import pprint
+import time
 
-from file_path_collect import output_pickle_path_dir as output, output_json_path_dir as json_output
+from file_path_collect import output_json_path_dir as json_output
 
-with open(output + 'entry.pickle', 'rb') as f:
-    entry = pickle.load(f)
-
-pprint(entry)
+entry = {'title': 'Divve into history, 2009 edition',
+         'article_link': 'http://diveintomark.org/archives/2009/03/27/dive‐into‐history‐2009‐edition',
+         'comments_link': None, 'internal_id': b'\xDE\xD5\xB4\xF8', 'tags': ('diveintopython', 'docbook', 'html'),
+         'published': True, 'published_date': time.strptime('Fri Mar 27 22:20:42 2009')}
 
 with open(json_output + 'entry.json', 'w', encoding='utf-8') as f:
     json.dump(entry, f)
 
 """
+/usr/local/bin/python3 /Users/moqi/Code/dive-into-python3-practice/c14/p413_json_wrong.py
 Traceback (most recent call last):
-  File "/Users/moqi/Code/dive-into-python3-practice/c14/p413_json_wrong.py", line 13, in <module>
+  File "/Users/moqi/Code/dive-into-python3-practice/c14/p413_json_wrong.py", line 12, in <module>
     json.dump(entry, f)
   File "/usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/lib/python3.7/json/__init__.py", line 179, in dump
     for chunk in iterable:
@@ -27,11 +27,6 @@ Traceback (most recent call last):
   File "/usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/lib/python3.7/json/encoder.py", line 179, in default
     raise TypeError(f'Object of type {o.__class__.__name__} '
 TypeError: Object of type bytes is not JSON serializable
-{'article_link': 'http://diveintomark.org/archives/2009/03/27/dive‐into‐history‐2009‐edition',
- 'comments_link': None,
- 'internal_id': b'\xde\xd5\xb4\xf8',
- 'published': True,
- 'published_date': time.struct_time(tm_year=2009, tm_mon=3, tm_mday=27, tm_hour=22, tm_min=20, tm_sec=42, tm_wday=4, tm_yday=86, tm_isdst=-1),
- 'tags': ('diveintopython', 'docbook', 'html'),
- 'title': 'Divve into history, 2009 edition'}
+
+Process finished with exit code 1
 """
--- c14/p417_json_wrong2.py
+++ c14/p417_json_wrong2.py
@@ -1,16 +1,16 @@
 import json
-import pickle
-from pprint import pprint
+import time
+
 from c14.p415_customerializer import to_json
+from file_path_collect import output_json_path_dir as json_output
 
-from file_path_collect import output_pickle_path_dir as output, output_json_path_dir as json_output
-
-with open(output + 'entry.pickle', 'rb') as f:
-    entry = pickle.load(f)
-
-pprint(entry)
+entry = {'title': 'Divve into history, 2009 edition',
+         'article_link': 'http://diveintomark.org/archives/2009/03/27/dive‐into‐history‐2009‐edition',
+         'comments_link': None, 'internal_id': b'\xDE\xD5\xB4\xF8', 'tags': ('diveintopython', 'docbook', 'html'),
+         'published': True, 'published_date': time.strptime('Fri Mar 27 22:20:42 2009')}
 
 with open(json_output + 'entry.json', 'w', encoding='utf-8') as f:
+    # time 对象现在已经可以被 dump 了，是一个 int 数组˚
     json.dump(entry, f, default=to_json)
 
 """
