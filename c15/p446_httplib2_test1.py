4c7f2b69ba5b5060b0db1e5d0a2857710a3e5d43
moqi <moqimoqidea@gmail.com>
2020-04-26 23:04:53 +0800

    20200426

--- c15/p446_httplib2_test1.py
+++ c15/p446_httplib2_test1.py
@@ -5,7 +5,8 @@ import httplib2
 from file_path_collect import output_cache_path_dir as cache
 
 h = httplib2.Http(cache)
-response, content = h.request('https://weibo.com/')
+weibo = 'https://weibo.com/'
+response, content = h.request(weibo)
 
 print(response.status)
 print()
@@ -14,6 +15,18 @@ pprint(content[:1000])
 print()
 
 print(len(content))
+print()
+
+# 第二遍，所有的内容和上次一样
+response2, content2 = h.request(weibo)
+
+print(response2.status)
+print()
+
+pprint(content2[:1000])
+print()
+
+print(len(content2))
 
 """
 200
@@ -33,6 +46,26 @@ print(len(content))
  b'     this[document.all ? "onreadystatechange" : "onload"] = null;\n      '
  b'              this.parentNode.removeChild(this);\n                    if '
  b'(c) {\n                        c()\n          ')
- 
+
+6117
+
+200
+
+(b'<!DOCTYPE html>\n<html>\n<head>\n    <meta http-equiv="Content-type" conten'
+ b't="text/html; charset=gb2312"/>\n    <title>Sina Visitor System</title>\n<'
+ b'/head>\n<body>\n<span id="message"></span>\n<script type="text/javascript" '
+ b'src="/js/visitor/mini_original.js?v=20161116"></script>\n<script type="te'
+ b'xt/javascript">\n    window.use_fp = "1" == "1"; // \xca\xc7\xb7\xf1\xb2'
+ b'\xc9\xbc\xaf\xc9\xe8\xb1\xb8\xd6\xb8\xce\xc6\xa1\xa3\n    var url = url || {'
+ b'};\n    (function () {\n        this.l = function (u, c) {\n            try'
+ b' {\n                var s = document.createElement("script");\n           '
+ b'     s.type = "text/javascript";\n                s[document.all ? "onrea'
+ b'dystatechange" : "onload"] = function () {\n\n                    if (docu'
+ b'ment.all && this.readyState != "loaded" && this.readyState != "complete"'
+ b') {\n                        return\n                    }\n               '
+ b'     this[document.all ? "onreadystatechange" : "onload"] = null;\n      '
+ b'              this.parentNode.removeChild(this);\n                    if '
+ b'(c) {\n                        c()\n          ')
+
 6117
 """
